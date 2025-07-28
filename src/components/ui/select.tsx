8494f70b29c27b27611ab54ef6f091b4b8e6a1c0
Julien Newman <juliennewman@juliens-air.home>
2025-07-28 14:58:31 -0400

    fixed color of input fields

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input2 bg-background2 px-3 py-2 text-sm ring-offset-background2 placeholder:text-muted-foreground2 focus:outline-none focus:ring-2 focus:ring-ring2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-10 w-full items-center justify-between rounded-md border border-gray-200 bg-background2 px-3 py-2 text-sm ring-offset-background2 placeholder:text-muted-foreground2 focus:outline-none focus:ring-2 focus:ring-ring2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
     {...props}
