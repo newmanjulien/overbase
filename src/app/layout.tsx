51b25503e595d9c427c303f7037911f417bde119
Julien Newman <juliennewman@juliens-air.home>
2025-09-17 16:21:14 -0400

    tweaked metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "hndl",
-  description: "Create and edit workflows",
+  title: "Overbase",
+  description: "Easiest Customer Success data",
   icons: "/favicon.ico",
 };
 
